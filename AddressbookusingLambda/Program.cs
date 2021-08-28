42804a97017d957a4036930d30f3e14b0e90194f
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-27 22:37:01 -0600

    Welcome to address book programs

---
+++ AddressbookusingLambda/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace AddressbookusingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book programs using Lambda expressions");
+        }
+    }
+}
